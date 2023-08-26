6d946604b89d1e50be1196ba4db67d9c55021f9b
Constantine <hellblazer@me.com>
2023-08-25 18:30:29 -0700

    kinetic tracking (#1)
    * Add first cut of sentinal for kinetic tracking of point clouds
    
    * tetrahedralize
    
    le sigh.  throw away and re tetrahedralize each and every time is
    literally the fastest.  lmao.
    
    * clean up
    
    * package rename
    
    * sim 500 with 1K iterations
    
    * peel back some more unneeded abstraction. add Prime Mover as dep
    
    some general cleanup n such

---
+++ sentinel/src/main/java/com/hellblazer/luciferase/sentinel/delaunay/Vertex.java
@@ -0,0 +1,277 @@
+/**
+ * Copyright (C) 2009 Hal Hildebrand. All rights reserved.
+ *
+ * This file is part of the 3D Incremental Voronoi system
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.hellblazer.luciferase.sentinel.delaunay;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import javax.vecmath.Point3f;
+import javax.vecmath.Tuple3d;
+import javax.vecmath.Tuple3f;
+import javax.vecmath.Vector3f;
+
+/**
+ *
+ * @author <a href="mailto:hal.hildebrand@gmail.com">Hal Hildebrand</a>
+ *
+ */
+public class Vertex extends Vector3f {
+    /**
+     * Minimal zero
+     */
+    static final double       EPSILON          = Math.pow(10F, -20F);
+    static final Point3f      ORIGIN           = new Point3f(0, 0, 0);
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Create some random points in a sphere
+     *
+     * @param random
+     * @param numberOfPoints
+     * @param radius
+     * @param inSphere
+     * @return
+     */
+    public static Point3f[] getRandomPoints(Random random, int numberOfPoints, float radius, boolean inSphere) {
+        double radiusSquared = radius * radius;
+        Point3f ourPoints[] = new Point3f[numberOfPoints];
+        for (int i = 0; i < ourPoints.length; i++) {
+            if (inSphere) {
+                do {
+                    ourPoints[i] = randomPoint(random, -radius, radius);
+                } while (ourPoints[i].distanceSquared(ORIGIN) >= radiusSquared);
+            } else {
+                ourPoints[i] = randomPoint(random, -radius, radius);
+            }
+        }
+
+        return ourPoints;
+    }
+
+    /**
+     * Generate a bounded random double
+     *
+     * @param random
+     * @param min
+     * @param max
+     * @return
+     */
+    public static float random(Random random, float min, float max) {
+        float result = random.nextFloat();
+        if (min > max) {
+            result *= min - max;
+            result += max;
+        } else {
+            result *= max - min;
+            result += min;
+        }
+        return result;
+    }
+
+    /**
+     * Generate a random point
+     *
+     * @param random
+     * @param min
+     * @param max
+     * @return
+     */
+    public static Point3f randomPoint(Random random, float min, float max) {
+        return new Point3f(random(random, min, max), random(random, min, max), random(random, min, max));
+    }
+
+    /**
+     * One of the tetrahedra adjacent to the vertex
+     */
+    private Tetrahedron adjacent;
+
+    public Vertex(float i, float j, float k) {
+        x = i;
+        y = j;
+        z = k;
+    }
+
+    public Vertex(float i, float j, float k, float scale) {
+        this(i * scale, j * scale, k * scale);
+    }
+
+    public Vertex(Tuple3f p) {
+        this(p.x, p.y, p.z);
+    }
+
+    public final double distanceSquared(Tuple3d p1) {
+        double dx, dy, dz;
+
+        dx = x - p1.x;
+        dy = y - p1.y;
+        dz = z - p1.z;
+        return dx * dx + dy * dy + dz * dz;
+    }
+
+    /**
+     * Answer one of the adjacent tetrahedron
+     *
+     * @return
+     */
+    public final Tetrahedron getAdjacent() {
+        return adjacent;
+    }
+
+    public LinkedList<OrientedFace> getEars() {
+        assert adjacent != null;
+        EarSet aggregator = new EarSet();
+        adjacent.visitStar(this, aggregator);
+        return aggregator.getEars();
+    }
+
+    /**
+     * Answer the collection of neighboring vertices around the indicated vertex.
+     *
+     * @param v - the vertex determining the neighborhood
+     * @return the collection of neighboring vertices
+     */
+    public Collection<Vertex> getNeighbors() {
+        assert adjacent != null;
+
+        final Set<Vertex> neighbors = new IdentitySet<>();
+        adjacent.visitStar(this, (vertex, t, x, y, z) -> {
+            neighbors.add(x);
+            neighbors.add(y);
+            neighbors.add(z);
+        });
+        return neighbors;
+    }
+
+    public Deque<OrientedFace> getStar() {
+        assert adjacent != null;
+
+        final Deque<OrientedFace> star = new ArrayDeque<>();
+        adjacent.visitStar(this, (vertex, t, x, y, z) -> {
+            star.push(t.getFace(vertex));
+        });
+        return star;
+    }
+
+    /**
+     * Answer the faces of the voronoi region around the receiver
+     *
+     * @param v - the vertex of interest
+     * @return the list of faces defining the voronoi region defined by the receiver
+     */
+    public List<Tuple3f[]> getVoronoiRegion() {
+        assert adjacent != null;
+
+        final List<Tuple3f[]> faces = new ArrayList<>();
+        Set<Vertex> neighbors = new IdentitySet<>(10);
+        adjacent.visitStar(this, (vertex, t, x, y, z) -> {
+            if (neighbors.add(x)) {
+                t.traverseVoronoiFace(this, x, faces);
+            }
+            if (neighbors.add(y)) {
+                t.traverseVoronoiFace(this, y, faces);
+            }
+            if (neighbors.add(z)) {
+                t.traverseVoronoiFace(this, z, faces);
+            }
+        });
+        return faces;
+    }
+
+    /**
+     * Return +1 if the receiver lies inside the sphere passing through a, b, c, and
+     * d; -1 if it lies outside; and 0 if the five points are cospherical. The
+     * vertices a, b, c, and d must be ordered so that they have a positive
+     * orientation (as defined by {@link #orientation(Vertex, Vertex, Vertex)}), or
+     * the sign of the result will be reversed.
+     * <p>
+     *
+     * @param a , b, c, d - the points defining the sphere, in oriented order
+     * @return +1 if the receiver lies inside the sphere passing through a, b, c,
+     *         and d; -1 if it lies outside; and 0 if the five points are
+     *         cospherical
+     */
+
+    public final int inSphere(Tuple3f a, Tuple3f b, Tuple3f c, Tuple3f d) {
+        double result = Geometry.inSphere(a.x, a.y, a.z, b.x, b.y, b.z, c.x, c.y, c.z, d.x, d.y, d.z, x, y, z);
+        if (result > 0.0) {
+            return 1;
+        } else if (result < 0.0) {
+            return -1;
+        }
+        return 0;
+    }
+
+    public void moveBy(Tuple3f delta) {
+        x = x + delta.x;
+        y = y + delta.y;
+        z = z + delta.z;
+    }
+
+    /**
+     * Answer +1 if the orientation of the receiver is positive with respect to the
+     * plane defined by {a, b, c}, -1 if negative, or 0 if the test point is
+     * coplanar
+     * <p>
+     *
+     * @param a , b, c - the points defining the plane
+     * @return +1 if the orientation of the query point is positive with respect to
+     *         the plane, -1 if negative and 0 if the test point is coplanar
+     */
+    public final int orientation(Tuple3f a, Tuple3f b, Tuple3f c) {
+        double result = Geometry.leftOfPlane(a.x, a.y, a.z, b.x, b.y, b.z, c.x, c.y, c.z, x, y, z);
+        if (result > 0.0) {
+            return 1;
+        } else if (result < 0.0) {
+            return -1;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + x + ", " + y + ", " + z + "}";
+    }
+
+    void freshenAdjacent(Tetrahedron tetrahedron) {
+        if (adjacent == null || adjacent.isDeleted())
+            adjacent = tetrahedron;
+    }
+
+    void reset() {
+        adjacent = null;
+    }
+
+    /**
+     * Note one of the adjacent tetrahedron
+     * <p>
+     *
+     * @param tetrahedron
+     */
+    final void setAdjacent(Tetrahedron tetrahedron) {
+        adjacent = tetrahedron;
+    }
+
+}
