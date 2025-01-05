ee5536b8ddb9c76104981c9290682ce1f0ceaae6
Hellblazer <hellblazer@me.com>
2025-01-05 12:35:25 -0800

    Tet api correct according to paper. break out tables from Tet record because aesthetics

--- lucien/src/main/java/com/hellblazer/luciferase/lucien/index/Tet.java
+++ lucien/src/main/java/com/hellblazer/luciferase/lucien/index/Tet.java
@@ -3,48 +3,14 @@ package com.hellblazer.luciferase.lucien.index;
 import javax.vecmath.Point3i;
 import javax.vecmath.Tuple3i;
 
+import static com.hellblazer.luciferase.lucien.index.TetConstants.*;
+
 /**
  * A tetrahedron in the mesh
  *
  * @author hal.hildebrand
  **/
-public record Tet(int x1, int x2, int x3, byte l, byte type) {
-
-    private static final byte[][] PARENT_2D = new byte[][] { { 0, 1 }, { 0, 0 }, { 1, 1 }, { 0, 1 } };
-    private static final byte[][] PARENT_3D = new byte[][] { { 0, 1, 2, 3, 4, 5 }, { 0, 1, 1, 1, 0, 0 },
-                                                             { 2, 2, 2, 3, 3, 3 }, { 1, 1, 2, 2, 2, 1 },
-                                                             { 5, 5, 4, 4, 4, 5 }, { 0, 0, 0, 5, 5, 5 },
-                                                             { 4, 3, 3, 3, 4, 4 }, { 0, 1, 2, 3, 4, 5 } };
-
-    private static final byte[][] CHILD_TYPE_TO_PARENT_TYPE = new byte[][] { { 0, 1 }, { 0, 0 }, { 1, 1 }, { 0, 1 } };
-
-    private static final byte[][] CHILD_2D = new byte[][] { { 0, 1, 3, 2 }, { 0, 2, 3, 1 } };
-    private static final byte[][] CHILD_3D = new byte[][] { { 0, 1, 4, 7, 2, 3, 6, 5 }, { 0, 1, 5, 7, 2, 3, 6, 4 },
-                                                            { 0, 3, 4, 7, 1, 2, 6, 5 }, { 0, 1, 6, 7, 2, 3, 4, 5 },
-                                                            { 0, 3, 5, 7, 1, 2, 4, 6 }, { 0, 3, 6, 7, 2, 1, 4, 5 } };
-
-    private static final byte[][] CHILD_TYPE_2D = new byte[][] { { 0, 0, 0, 1 }, { 1, 1, 1, 0 } };
-    private static final byte[][] CHILD_TYPE_3D = new byte[][] { { 0, 0, 0, 0, 4, 5, 2, 1 }, { 1, 1, 1, 1, 3, 2, 5, 0 },
-                                                                 { 2, 2, 2, 2, 0, 1, 4, 3 }, { 3, 3, 3, 3, 5, 4, 1, 2 },
-                                                                 { 4, 4, 4, 4, 2, 3, 0, 5 },
-                                                                 { 5, 5, 5, 5, 1, 0, 3, 4 } };
-
-    private static final byte[][] TRI_INDEX_TO_BEYS    = { { 0, 1, 3, 2 }, { 0, 3, 1, 2 } };
-    private static final byte[]   TRI_BEY_ID_TO_VERTEX = { 0, 1, 2, 1 };
-
-    private static final byte[][] TRI_TYPE_OF_CHILD        = { { 0, 0, 0, 1 }, { 1, 1, 1, 0 } };
-    private static final byte[][] TRI_TYPE_OF_CHILD_MORTON = { { 0, 0, 1, 0 }, { 1, 0, 1, 1 } };
-
-    private static final Tuple3i[][] BASIC_TYPE_2D = new Tuple3i[][] {
-    { CORNER.c0.coords(), CORNER.c1.coords(), CORNER.c3.coords() },
-    { CORNER.c0.coords(), CORNER.c2.coords(), CORNER.c3.coords() } };
-    private static final Tuple3i[][] BASIC_TYPE_3D = new Tuple3i[][] {
-    { CORNER.c0.coords(), CORNER.c1.coords(), CORNER.c5.coords(), CORNER.c7.coords() },
-    { CORNER.c0.coords(), CORNER.c1.coords(), CORNER.c3.coords(), CORNER.c7.coords() },
-    { CORNER.c0.coords(), CORNER.c2.coords(), CORNER.c3.coords(), CORNER.c7.coords() },
-    { CORNER.c0.coords(), CORNER.c2.coords(), CORNER.c6.coords(), CORNER.c7.coords() },
-    { CORNER.c0.coords(), CORNER.c4.coords(), CORNER.c6.coords(), CORNER.c7.coords() },
-    { CORNER.c0.coords(), CORNER.c4.coords(), CORNER.c5.coords(), CORNER.c7.coords() } };
+public record Tet(int x, int y, int z, byte l, byte type) {
 
     /**
      * @param L - maximum refinement level
@@ -55,31 +21,41 @@ public record Tet(int x1, int x2, int x3, byte l, byte type) {
     }
 
     /**
-     * Answer the 3D coordinates of the vertex
+     * Answer the 3D coordinates of the tetrahedron represented by the receiver
      *
-     * @param L      - max refinement level
-     * @param vertex - the vertex #
-     * @return the 3D coordinates of the vertex
+     * @param L - max refinement level
+     * @return the 3D coordinates of the tetrahedron described by the receiver in CCW order
      */
-    public Tuple3i coordinates(int L, int vertex) {
-        int h = lengthAtLevel(L);
-        int ei = type / 2;
+    public Tuple3i[] coordinates(int L) {
+        var coords = new Point3i[4];
+        coords[0] = new Point3i(x, y, z);
+        var h = lengthAtLevel(L);
+        var i = type / 2;
+        var j = 0;
+        if (type / 2 == 0) {
+            j = (i + 2) % 3;
+        } else {
+            j = (i + 1) % 3;
+        }
 
-        var coords = new int[] { x1, x2, x3 };
-        if (vertex == 0) {
-            return new Point3i(coords[0], coords[1], coords[2]);
+        if (i == 0) {
+            coords[1] = new Point3i(coords[0].x + h, coords[0].y, coords[0].z);
+        } else if (i == 1) {
+            coords[1] = new Point3i(coords[0].x, coords[0].y + h, coords[0].z);
+        } else if (i == 2) {
+            coords[2] = new Point3i(coords[0].x, coords[0].y, coords[0].z + h);
         }
-        coords[ei] += h;
-        if (vertex == 2) {
-            int ej = (ei + ((type % 2 == 0) ? 2 : 1)) % 3;
-            coords[1 - ej] += h;
-            return new Point3i(coords[0], coords[1], coords[2]);
+
+        if (j == 0) {
+            coords[2] = new Point3i(coords[1].x + h, coords[1].y, coords[1].z);
+        } else if (j == 1) {
+            coords[2] = new Point3i(coords[1].x, coords[1].y + h, coords[1].z);
+        } else if (j == 2) {
+            coords[2] = new Point3i(coords[1].x, coords[1].y, coords[1].z + h);
         }
-        if (vertex == 3) {
-            coords[(ei + 1) % 3] += h;
-            coords[(ei + 2) % 3] += h;
-        }
-        return new Point3i(coords[0], coords[1], coords[2]);
+
+        coords[3] = new Point3i(coords[1].x + h, coords[1].y + h, coords[1].z + h);
+        return coords;
     }
 
     /**
@@ -90,11 +66,11 @@ public record Tet(int x1, int x2, int x3, byte l, byte type) {
         if (l == 0) {
             return 0;
         }
-        int i = 0;
         int h = lengthAtLevel(L);
-        i |= (x1 & h) > 0 ? 1 : 0;
-        i |= (x2 & h) > 0 ? 2 : 0;
-        i |= (x3 & h) > 0 ? 4 : 0;
+        int i = 0;
+        i |= (x & h) > 0 ? 1 : 0;
+        i |= (y & h) > 0 ? 2 : 0;
+        i |= (z & h) > 0 ? 4 : 0;
         return i;
     }
 
@@ -104,7 +80,7 @@ public record Tet(int x1, int x2, int x3, byte l, byte type) {
      */
     public Tet parent(int L) {
         int h = lengthAtLevel(L);
-        return new Tet(x1 & ~h, x2 & ~h, x3 & ~h, (byte) (l - 1), PARENT_3D[cubeId(L)][type]);
+        return new Tet(x & ~h, y & ~h, z & ~h, (byte) (l - 1), PARENT_3D[cubeId(L)][type]);
     }
 
     /**
@@ -112,21 +88,18 @@ public record Tet(int x1, int x2, int x3, byte l, byte type) {
      * @return the i-th child (in Bey's order) of the receiver
      */
     public Tet child(int i, int L) {
-        var coords = new int[] { x1, x2, x3 };
-        var bey = TRI_INDEX_TO_BEYS[type][i];
-        if (bey == 0) {
-            coords[0] = x1;
-            coords[1] = x2;
-            coords[2] = x3;
-        } else {
-            /* i-th anchor coordinate of child is (X_(0,i)+X_(vertex,i))/2
-             * where X_(i,j) is the j-th coordinate of t's ith node */
-            var coordinates = coordinates(L, TRI_BEY_ID_TO_VERTEX[bey]);
-            coords[0] = x1 + coordinates.x >> 1;
-            coords[1] = x2 + coordinates.y >> 1;
-            coords[2] = x1 + coordinates.z >> 1;
+        var coords = coordinates(L);
+        var j = 0;
+        if (i == 1 || i == 4 || i == 5) {
+            j = 1;
+        } else if (i == 2 || i == 6 || i == 7) {
+            j = 2;
         }
-        return new Tet(coords[0], coords[1], coords[2], (byte) (l - 1), TRI_TYPE_OF_CHILD[type][bey]);
+        if (i == 3) {
+            j = 3;
+        }
+        return new Tet((coords[0].x + coords[j].x) / 2, (coords[0].x + coords[j].x) / 2,
+                       (coords[0].x + coords[j].x) / 2, (byte) (l + 1), CHILD_TYPE_3D[type][i]);
     }
 
     /**
@@ -134,53 +107,6 @@ public record Tet(int x1, int x2, int x3, byte l, byte type) {
      * @return the i-th child (in Tet Morton order) of the receiver
      */
     public Tet childTM(int i, int L) {
-        return child(TRI_TYPE_OF_CHILD_MORTON[type][i], L);
-    }
-
-    // The corners of a cube
-    private enum CORNER {
-        c0 {
-            @Override
-            public Tuple3i coords() {
-                return new Point3i(0, 0, 0);
-            }
-        }, c1 {
-            @Override
-            public Tuple3i coords() {
-                return new Point3i(1, 0, 0);
-            }
-        }, c2 {
-            @Override
-            public Tuple3i coords() {
-                return new Point3i(0, 1, 0);
-            }
-        }, c3 {
-            @Override
-            public Tuple3i coords() {
-                return new Point3i(1, 1, 0);
-            }
-        }, c4 {
-            @Override
-            public Tuple3i coords() {
-                return new Point3i(0, 0, 1);
-            }
-        }, c5 {
-            @Override
-            public Tuple3i coords() {
-                return new Point3i(1, 0, 1);
-            }
-        }, c6 {
-            @Override
-            public Tuple3i coords() {
-                return new Point3i(0, 1, 1);
-            }
-        }, c7 {
-            @Override
-            public Tuple3i coords() {
-                return new Point3i(1, 1, 1);
-            }
-        };
-
-        abstract public Tuple3i coords();
+        return child(CHILD_TYPE_3D_MORTON[type][i], L);
     }
 }
---
+++ lucien/src/main/java/com/hellblazer/luciferase/lucien/index/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author hal.hildebrand
+ *
+ * Implements the spatial index from the paper:
+ * <a href="https://arxiv.org/abs/1509.04627">A tetrahedral space-filling curve for non-conforming adaptive meshes</a>
+ */
+package com.hellblazer.luciferase.lucien.index;
